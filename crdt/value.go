c3109e26c230b7e3cf77d2e7a0678854ae230512
jrhy <jrhy>
2022-11-19 22:28:13 -0800

    add crdt/value.go

---
+++ crdt/value.go
@@ -0,0 +1,35 @@
+package crdt
+
+type Value struct {
+	ModEpochNanos            int64       `json:"m"`
+	PreviousRoot             string      `json:"p,omitempty"`
+	TombstoneSinceEpochNanos int64       `json:"d,omitempty"`
+	Value                    interface{} `json:"v"`
+}
+
+func (v Value) Tombstoned() bool {
+	return v.TombstoneSinceEpochNanos != 0
+}
+
+func LastWriteWins(newValue, oldValue *Value) *Value {
+	if newValue.Tombstoned() || oldValue.Tombstoned() {
+		return firstTombstoneWins(newValue, oldValue)
+	}
+	if newValue.ModEpochNanos > oldValue.ModEpochNanos {
+		return newValue
+	}
+	return oldValue
+}
+
+func firstTombstoneWins(newValue, oldValue *Value) *Value {
+	if !newValue.Tombstoned() {
+		return oldValue
+	}
+	if !oldValue.Tombstoned() {
+		return newValue
+	}
+	if newValue.TombstoneSinceEpochNanos < oldValue.TombstoneSinceEpochNanos {
+		return newValue
+	}
+	return oldValue
+}
