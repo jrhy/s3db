061be9499479e150cf5dd8c33535b381b4c11693
jrhy <jrhy>
2023-07-25 22:08:21 -0700

    replace internal/experimental/go-sqlite3 with sqlite/mod for use inside Go programs

--- sqlite/example-go-app/fun.go
+++ sqlite/example-go-app/fun.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"database/sql"
+	"errors"
+	"fmt"
 
 	// register s3db extension with riyazali.net/sqlite
 	_ "github.com/jrhy/s3db/sqlite/mod"
@@ -31,5 +33,30 @@ func run() error {
 	if err != nil {
 		return err
 	}
+	_, err = db.Exec("insert into f values(1,1)")
+	if err != nil {
+		return err
+	}
+	r, err := db.Query("select * from f")
+	if err != nil {
+		return err
+	}
+	if !r.Next() {
+		return errors.New("something went wrong! where did the first row go?")
+	}
+	if cols, err := r.Columns(); err != nil {
+		return err
+	} else {
+		fmt.Printf("columns: %+v\n", cols)
+	}
+	var a, b int64
+	err = r.Scan(&a, &b)
+	if err != nil {
+		return err
+	}
+	if a != int64(1) || b != int64(1) {
+		return fmt.Errorf("something went wrong! the first row should be (1,1) but is (%d,%d)!", a, b)
+	}
+	fmt.Printf("the first row is: [%d %d]\n", a, b)
 	return nil
 }
